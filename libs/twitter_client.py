f5237d77843e3186da339920ad82b4fb6b5ecbf2
lumiereconseil <lumiere.conseil@outlook.com>
2026-08-24 18:19:25 +0900

    Implement TwitterClient class for posting tweets

--- libs/twitter_client.py
+++ libs/twitter_client.py
@@ -1,1 +1,21 @@
+import requests
+import os
+
+class TwitterClient:
+    def __init__(self):
+        self.api_key = os.getenv("X_API_KEY")
+        self.api_secret = os.getenv("X_API_SECRET")
+        self.access_token = os.getenv("X_ACCESS_TOKEN")
+        self.access_secret = os.getenv("X_ACCESS_SECRET")
+
+    def post_tweet(self, text: str):
+        url = "https://api.twitter.com/2/tweets"
+        headers = {
+            "Authorization": f"Bearer {self.access_token}",
+            "Content-Type": "application/json"
+        }
+        payload = {"text": text}
+
+        response = requests.post(url, json=payload, headers=headers)
+        return response.json()
 
