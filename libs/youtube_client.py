838f675d7938a0482e98c8224367109e0d352772
lumiereconseil <lumiere.conseil@outlook.com>
2026-08-24 18:20:18 +0900

    Update youtube_client.py

--- libs/youtube_client.py
+++ libs/youtube_client.py
@@ -1,1 +1,18 @@
+import os
+from googleapiclient.discovery import build
+
+class YouTubeClient:
+    def __init__(self):
+        self.api_key = os.getenv("YT_API_KEY")
+        self.youtube = build("youtube", "v3", developerKey=self.api_key)
+
+    def search_video(self, query: str):
+        request = self.youtube.search().list(
+            part="snippet",
+            q=query,
+            type="video",
+            maxResults=1
+        )
+        response = request.execute()
+        return response
 
